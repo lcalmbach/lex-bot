fe899be6e59c189994a29f9a573a71d6bf9d361a
godot63 <lcalmbach@gmail.com>
2025-01-02 15:41:16 +0100

    correct typos in info text

--- texts.py
+++ texts.py
@@ -1,12 +1,13 @@
 info = """# Lex-Bot ⚖️
-Diese App bietet die Möglichkeit, die Gesetzessammlung des Kantons Basel-Stadt zu durchsuchen und gezielt Fragen zu stellen. Alle Texte sind in einer Datenbank als Vektoren abgelegt. In diesem Format können Fragen sehr effizient mit den Gesetzestexten verglichen werden und nach Texten gefiltert werden, die am wahrscheinlichsten die Antwrten auf die Frage enthalten. Diese Dokumente werden anschliessend an ein Large Langauge Model geschickt und, welches die Antwort auf die Frage basierend auf diese mitgelieferten Dokumente zurückschickt. Die Antwort des LLMs kann inkorrekt sein, kontrolliere im Zweifelsfalle immer die mitgelieferten Dokumente, welche unter der Antwort angezeigt werden. 
+Diese App bietet die Möglichkeit, die Gesetzessammlung des Kantons Basel-Stadt zu durchsuchen und gezielt Fragen zu stellen. Alle Texte sind in einer Datenbank als Vektoren abgelegt. In diesem Format können Fragen sehr effizient mit den Gesetzestexten verglichen und nach Texten gefiltert werden, die am wahrscheinlichsten die Antworten auf die Frage enthalten. Diese Dokumente werden anschliessend an ein Large Language Model (LLM) übermittelt, das auf Basis dieser Dokumente eine Antwort generiert. Die Antwort des LLMs kann jedoch inkorrekt sein. Kontrolliere im Zweifelsfall immer die mitgelieferten Dokumente, welche unter der Antwort angezeigt werden.
 
-- Der Menüpunkt **Gesetze** erlaubt eine hierarchische Darstellung und detaillierte Suche nach Gesetzestexten.  
-- Mit **Lex-Chat** kannst du Fragen zu den Gesetzen stellen und passende Antworten erhalten.  
+Der Menüpunkt Gesetze erlaubt eine hierarchische Darstellung und detaillierte Suche nach Gesetzestexten.
 
-Die zugrunde liegenden Daten basieren auf der [Systematischen Gesetzessammlung](https://www.gesetzessammlung.bs.ch/app/de/systematic/texts_of_law) sowie den Erlasstexten des Kantons Basel-Stadt. Sie ermöglichen eine systematische Kategorisierung und Beschreibung der geltenden Rechtsnormen. Änderungen von Erlassen (Chronologische Gesetzessammlung) sind ebenfalls verfügbar, entweder unter [gesetzessammlung.bs.ch](https://www.gesetzessammlung.bs.ch/app/de/chronology/change_documents) oder direkt im Datensatz: [Chronologische Gesetzessammlung](https://data.bs.ch/explore/dataset/100355/).  
+Mit Lex-Chat kannst du Fragen zu den Gesetzen stellen und passende Antworten erhalten.
 
-Alle Daten werden von der [Open Data Plattform](https://data.bs.ch/) des Kantons Basel-Stadt bereitgestellt. Der Datensatz enthält {} Dokumente. 
+Die zugrunde liegenden Daten basieren auf der Systematischen Gesetzessammlung sowie den Erlasstexten des Kantons Basel-Stadt. Sie ermöglichen eine systematische Kategorisierung und Beschreibung der geltenden Rechtsnormen. Änderungen von Erlassen (Chronologische Gesetzessammlung) sind ebenfalls verfügbar, entweder unter gesetzessammlung.bs.ch oder direkt im Datensatz: Chronologische Gesetzessammlung.
+
+Alle Daten werden von der Open Data Plattform des Kantons Basel-Stadt bereitgestellt. Der Datensatz enthält {} Dokumente.
 """
 
 default_question = "Wer darf zu Statistik Zwecken Daten verknüpfen?"
